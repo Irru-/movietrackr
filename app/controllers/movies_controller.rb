f84d34f377d16464ee5fabf10a13b70549a88fb9
Nick Gobee <nickgobee0122@gmailcom>
2014-09-02 10:16:07 +0200

    Added date field

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,11 +4,13 @@ class MoviesController < ApplicationController
 	end
 
 	def all
-		@movie = Movie.all
+		@movie = Movie.order('rating DESC')
 	end
 
 	def create
+		swap_date(movie_params)
 		@movie = Movie.new(movie_params)
+
 		if @movie.save
 			redirect_to '/movies/list'
 		else
@@ -19,6 +21,13 @@ class MoviesController < ApplicationController
 	private
 
 		def movie_params
-			params.require(:movie).permit(:title, :rating)
+			params.require(:movie).permit(:title, :rating, :saw_movie_at)
+		end
+
+		def swap_date(movie_params)
+			temp = movie_params["saw_movie_at(1i)"]
+			movie_params["saw_movie_at(1i)"] = movie_params["saw_movie_at(3i)"]
+			movie_params["saw_movie_at(3i)"] = temp
+			movie_params
 		end
 end
