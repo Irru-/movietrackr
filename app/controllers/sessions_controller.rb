15e135e4a2847d67f2778c02e50017b8d82eb376
Nick Gobee <nickgobee0122@gmailcom>
2014-09-10 17:08:22 +0200

    EOD 10/9/14

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,25 @@
 require 'bcrypt'
 
 class SessionsController < ApplicationController
-	include BCrypt
+    include BCrypt
 
-	def new
-	end
+    def new
+    end
 
-	def create
-		@user = User.find_by(username: params[:session][:username])
-		@user = User.find_by(email: params[:session][:username]) if @user.nil?
-		pwcheck = params[:session][:password]
-		if (!@user.nil? && @user.password == pwcheck)
-			sign_in @user
-			redirect_to root_url
-		else
-      		render 'new'
-		end
-	end
+    def create
+        @user = User.find_by(username: params[:session][:username])
+        @user = User.find_by(email: params[:session][:username]) if @user.nil?
+        pwcheck = params[:session][:password]
+        if (!@user.nil? && @user.password == pwcheck)
+            sign_in @user
+            redirect_to root_url
+        else
+            render 'new'
+        end
+    end
 
-	def destroy
-		sign_out
-    	redirect_to root_url
-	end
+    def destroy
+        sign_out
+        redirect_to root_url
+    end
 end
